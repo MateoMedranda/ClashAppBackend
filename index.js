79d26b77e26cf4174a697c2439bbc6291e3117c7
Mateo Medranda <matemedranda15@gmail.com>
2025-12-06 11:27:59 -0500

    V1.2.0

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // --- CONFIGURACIÓN ---
 // Pon aquí tu token de Clash Royale (el que creaste en la web)
-const CLASH_API_KEY = "TU_TOKEN_API_AQUI_MUY_LARGO";
+const CLASH_API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImY4ZjkyOWFhLWU2MTEtNGZhYi04MDhhLTg3Njk3YmY4MmZiYyIsImlhdCI6MTc2NTAzNzI5Mywic3ViIjoiZGV2ZWxvcGVyL2Y3YzI3ZWY1LTA2YTktOTQ0Yi01NTc5LWY0NDFlMzE0OTA4NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODEuMjI0LjE3NC40NSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.UQrhViUPvIaE_x-iTAXODMNXvbIatSrrHClh_WIfsJoGe7JtYKOMZoUSbIddBcL1HvPNpJMJXS7lVqwipcpw6A";
 const BASE_URL = "https://api.clashroyale.com/v1";
 
 // --- RUTA (ENDPOINT) ---
@@ -56,6 +56,30 @@ app.get('/api/clans', async (req, res) => {
         res.status(500).json({ error: 'Error al buscar clanes' });
     }
 });
+
+// ... tus otras rutas ...
+
+// --- RUTA UTILITARIA: VER MI IP ---
+// Entra aquí desde tu navegador para saber qué IP poner en Clash Royale
+app.get('/api/check-ip', async (req, res) => {
+    try {
+        // Le preguntamos a ipify.org cuál es nuestra IP pública
+        const response = await axios.get('https://api.ipify.org?format=json');
+        
+        const ip = response.data.ip;
+        console.log("La IP de salida de Render es:", ip);
+        
+        res.json({
+            mensaje: "Copia esta IP y pégala en el panel de Clash Royale",
+            ip_detectada: ip
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "No pude detectar la IP" });
+    }
+});
+
+
 // --- INICIAR SERVIDOR ---
 // Usa el puerto que le asigne la nube (process.env.PORT) o el 3000 si es local
 const PORT = process.env.PORT || 3000;
